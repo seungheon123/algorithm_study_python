e2b324985e6b5a2a866997a8fc50073a323fe660
한승헌 <seungheon123@khu.ac.kr>
2023-03-14 20:50:28 +0900

    백준 1927

---
+++ 1927.py
@@ -0,0 +1,16 @@
+import heapq
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+heap = []
+for _ in range(n):
+    x = int(input())
+    if x == 0: 
+        if not heap:
+            print(0)
+            continue
+        else:
+            print(heapq.heappop(heap))
+            continue
+    heapq.heappush(heap,x)
