2a07c88652e9359d7307b8e902f63617ad14b233
한승헌 <seungheon123@khu.ac.kr>
2023-03-14 21:23:33 +0900

    백준 11279

---
+++ 11279.py
@@ -0,0 +1,15 @@
+import heapq
+import sys
+n = int(sys.stdin.readline())
+heap = []
+for _ in range(n):
+    x = int(sys.stdin.readline())
+    if x == 0:
+        if not heap: 
+            print(0)
+            continue
+        else:
+            print(heapq.heappop(heap)[1])
+            continue
+    
+    heapq.heappush(heap,(-x,x))
